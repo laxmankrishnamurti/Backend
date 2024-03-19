5e5f64e9471ca35a0d5b1feacb5b73a907d1a22a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-03-19 15:25:30 +0530

    Making changes while revision

--- Class-04/db/index.js
+++ Class-04/db/index.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`);
         console.log('MongoDB connection is successfull !!!');
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
     } catch (error) {
-        console.log('MongoDB connection error :', error);
+        console.log('MongoDB connection error;', error);
         process.exit(1);
     }
 }
