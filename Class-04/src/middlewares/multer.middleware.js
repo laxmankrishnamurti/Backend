482ffb685c5dd47e1e20ff17e2ba825a1a2ea0d6
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-03-25 09:36:59 +0530

    Adding file uoloader file's

---
+++ Class-04/src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname) //try to console the file method.
+    }
+})
+
+export const upload = multer({
+    // storage: storage   (if both are equall then we can simply write it at one time. {es6 feature})
+    storage
+})
---
+++ Class-04/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        const response = await cloudinary.uploader.upload(
+            localFilePath,
+            {
+                resource_type: "auto"
+            }
+        )
+        console.log(`File is Uploaded on Cloudinary ${response.url}`)
+        return response;
+    } catch (error) {
+        // Remove the locally saved temporary file as the upload operation got failed.
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
