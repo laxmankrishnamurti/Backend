8e0d433f4f90c302568557fac2a4c628bd157d10
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-04-08 19:15:35 +0530

    Adding new files

--- Introduction to Backend/Class-01/index.js
+++ Introduction to Backend/Class-01/index.js
@@ -9,18 +9,35 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += '/index.html'
+            res.statusCode = 200
             break;
 
         case '/about':
             path += '/about.html'
+            res.statusCode = 200
+            break;
+
+        //Redirection page 
+
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('location', '/about')
+            res.end()
             break;
 
         case '/contact':
             path += '/contact.html'
+            res.statusCode = 200
+            break;
+
+        case '/confirm':
+            path += '/confirmation.html'
+            res.statusCode = 200
             break;
 
         default:
             path += '/notFound.html'
+            res.statusCode = 404
             break;
     }
 
