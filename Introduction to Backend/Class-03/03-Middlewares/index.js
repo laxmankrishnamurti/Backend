531134d06e8c444bd9cf1f92064a4b0509791ebb
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-04-10 20:04:39 +0530

    Deleting node module file

---
+++ Introduction to Backend/Class-03/03-Middlewares/index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+
+const app = express()
+const PORT = 4044
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+const homeRoute = express.Router()
+const userRoute = express.Router()
+
+app.use('/', homeRoute)
+app.use('/user', userRoute)
+
+homeRoute
+    .route('/')
+    .get(getHomePage)
+
+homeRoute
+    .route('/login')
+    .get(getLoginPage)
+    .post(getPassword)
+
+userRoute
+    .route('/course')
+    .get(middle, getCourse)
+
+function getHomePage(req, res) {
+    res.sendFile('./pages/index.html', {
+        root: __dirname
+    })
+}
+
+function getLoginPage(req, res) {
+    res.sendFile('./pages/login.html', {
+        root: __dirname
+    })
+}
+
+function getPassword(req, res) {
+    let data = req.body
+    console.log(data)
+    if (data.password === '1234') {
+        res.send(data)
+    }
+}
+
+function getCourse(req, res) {
+    res.sendFile('./pages/course.html', {
+        root: __dirname
+    })
+}
+
+function middle(req, res, next) {
+    console.log('middleware one is successfully executed')
+    res.redirect('/login')
+    next()
+}
+
+app.listen(PORT, () => {
+    console.log(`server is listening on http://localhost:${PORT}`)
+})
