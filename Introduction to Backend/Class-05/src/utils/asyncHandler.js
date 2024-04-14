89b9b0d90dc58d29a10518c3a01bba304bd10c65
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-04-14 13:32:52 +0530

    Adding utils file's

---
+++ Introduction to Backend/Class-05/src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (fun) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(fun(req, res, next))
+            .catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
