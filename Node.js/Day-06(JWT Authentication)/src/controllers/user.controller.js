f49c46b9e85d9241a7ef18884764c5da8dcce852
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-10 04:30:31 +0530

    Making changes in the file

--- Node.js/Day-06(JWT Authentication)/src/controllers/user.controller.js
+++ Node.js/Day-06(JWT Authentication)/src/controllers/user.controller.js
@@ -1,70 +1,68 @@
-const USER = require('../models/user.model')
-const {setUser} = require('../utils/authUser.utils')
-const {v4: uuidv4} = require('uuid')
+const USER = require("../models/user.model");
+const { setUser } = require("../utils/authUser.utils");
+// const { v4: uuidv4 } = require("uuid");
 
-async function handleUserRegistration(req, res){
-    const userPayload = req.body
+async function handleUserRegistration(req, res) {
+  const userPayload = req.body;
 
-    if(!userPayload.name || !userPayload.email || !userPayload.password){
-        return res.render("userRegistration", {
-            status: "missing",
-            msg: "All fields are required"
-        })
-    }
+  if (!userPayload.name || !userPayload.email || !userPayload.password) {
+    return res.render("userRegistration", {
+      status: "missing",
+      msg: "All fields are required",
+    });
+  }
 
-    const existUser = await USER.findOne({"email": userPayload.email})
+  const existUser = await USER.findOne({ email: userPayload.email });
 
-    if(existUser){
-        res.render("userRegistration", {
-            status: "exist",
-            msg: "user already exist, Please login"
-        })
-    }
+  if (existUser) {
+    res.render("userRegistration", {
+      status: "exist",
+      msg: "user already exist, Please login",
+    });
+  }
 
-    const createUserAccount = await USER.create({
-        name: userPayload.name,
-        email: userPayload.email,
-        password: userPayload.password
-    })
+  const createUserAccount = await USER.create({
+    name: userPayload.name,
+    email: userPayload.email,
+    password: userPayload.password,
+  });
 
-    if(createUserAccount){
-        res.redirect('/url')
-    }else {
-        res.render("userRegistration", {
-            "msg": "Internal server error, please try after some time"
-        })
-    }
-
+  if (createUserAccount) {
+    res.redirect("/url");
+  } else {
+    res.render("userRegistration", {
+      msg: "Internal server error, please try after some time",
+    });
+  }
 }
 
+async function handleUserLogin(req, res) {
+  const { name, password } = req.body;
 
-async function handleUserLogin(req, res){
-    const {name, password} = req.body
+  if (!name || !password) {
+    res.render("login", {
+      status: "missing",
+      msg: "All fields are required",
+    });
+  }
 
-    if(!name || !password){
-        res.render("login", {
-            status:"missing",
-            msg: "All fields are required"
-        })
-    }
+  //   const sessionId = uuidv4();  //too old
+  const loginUser = await USER.findOne({ name, password });
 
-    const sessionId = uuidv4();
-    const loginUser = await USER.findOne({name, password});
+  if (!loginUser) {
+    return res.status(400).render("login", {
+      status: "nouser",
+      msg: "user dosen't exist, please signup",
+    });
+  }
 
-    setUser(sessionId, loginUser)
+  setUser(loginUser);
 
-    res.cookie("sessionId", sessionId);
+  res.cookie("sessionId", sessionId);
 
-    if(loginUser){
-        res.redirect('/')
-    }else{
-        res.render("login", {
-            status: "nouser",
-            msg: "user dosen't exist, please signup"
-        })
-    }
-
+  if (loginUser) {
+    return res.redirect("/");
+  }
 }
 
-
-module.exports = {handleUserRegistration, handleUserLogin}
+module.exports = { handleUserRegistration, handleUserLogin };
--- Node.js/Day-06(JWT Authentication)/src/utils/authUser.utils.js
+++ Node.js/Day-06(JWT Authentication)/src/utils/authUser.utils.js
@@ -1,12 +1,36 @@
-const sessingToUserMap = new Map()
+const jwt = require("jsonwebtoken");
 
-function setUser(id, user){
-    sessingToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET_KEY
+  );
 }
 
-function getUser(id){
-    sessingToUserMap.get(id);
+function getUser(token) {
+  return jwt.verify(token, process.env.JWT_SECRET_KEY);
 }
 
+module.exports = {
+  setUser,
+  getUser,
+};
 
-module.exports = { setUser, getUser }
+/**
+ * Statefull sessionID
+ */
+
+// const sessingToUserMap = new Map()
+
+// function setUser(id, user){
+//     sessingToUserMap.set(id, user);
+// }
+
+// function getUser(id){
+//     sessingToUserMap.get(id);
+// }
+
+// module.exports = { setUser, getUser }
