39368241466f320b707e8bc73bd1ed46d20a826f
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-03 03:26:07 +0530

    Assignment two has been completed

--- Node.JS Assignments/Chapter-02/Assignment-02/index.js
+++ Node.JS Assignments/Chapter-02/Assignment-02/index.js
@@ -1,20 +1,40 @@
 const http = require("http");
 
-let PORT_1 = 3001;
-let PORT_2 = 3002;
+const startServer = (port) => {
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`Server running on ${port}`);
+  });
 
-const serverOne = http.createServer((req, res) => {
-  res.end("<h1>Hello from first server</h1>");
-});
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+};
 
-const serverTwo = http.createServer((req, res) => {
-  res.end("<h1>Hello from second server</h1>");
-});
+const port1 = process.argv[2] || 3000;
+const port2 = process.argv[3] || 3000;
 
-serverOne.listen(PORT_1, () => {
-  console.log(`serverOne is listening on http://localhost:${PORT_1}`);
-});
+startServer(port1);
+startServer(port2);
 
-serverTwo.listen(PORT_2, () => {
-  console.log(`serverTwo is listening on http://localhost:${PORT_2}`);
-});
+// const http = require("http");
+
+// let PORT_1 = 3001;
+// let PORT_2 = 3002;
+
+// const serverOne = http.createServer((req, res) => {
+//   res.end("<h1>Hello from first server</h1>");
+// });
+
+// const serverTwo = http.createServer((req, res) => {
+//   res.end("<h1>Hello from second server</h1>");
+// });
+
+// serverOne.listen(PORT_1, () => {
+//   console.log(`serverOne is listening on http://localhost:${PORT_1}`);
+// });
+
+// serverTwo.listen(PORT_2, () => {
+//   console.log(`serverTwo is listening on http://localhost:${PORT_2}`);
+// });
